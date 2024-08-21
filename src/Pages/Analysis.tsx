0dd80c28eef709b4c124664c4f8a664d3172ed50
aaronjoju07 <aaronjoju07@gmail.com>
2024-08-21 11:56:27 +0530

    "Refactor Analysis page layout to use Flex components and remove StatsTitleDescription"

--- src/Pages/Analysis.tsx
+++ src/Pages/Analysis.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
-import StatsTitleDescription from '../Components/statsTitleDescription';
+import { Box, Flex } from '@chakra-ui/react';
 
 const Analysis: React.FC = () => {
   return (
-    <Box p={4}>
-        <StatsTitleDescription />
-    </Box>
+    <Flex
+      minH="100vh"
+      p={5}
+      pt={20}
+      gap={4}
+      alignItems="center"
+      bg="gray.100"
+    >
+      <Box w="25%" display="flex" flexDirection="column" gap={5}>
+        <Box h="20%" bg="#E2E8F0">asd</Box>
+        <Box h="20%" bg="#F7FAFC">asda</Box>
+        <Box h="60%" bg="#F7FAFC">asd</Box>
+      </Box>
+
+      <Box w="25%" display="flex" flexDirection="column" gap={5}>
+        <Box h="40%" bg="#F7FAFC">asd</Box>
+        <Box h="60%" bg="#F7FAFC">asd</Box>
+      </Box>
+
+      <Box w="50%" display="flex" flexDirection="column" gap={5}>
+        <Box h="72" bg="#F7FAFC"></Box>
+        <Flex gap={5}>
+          <Box w="50%" h="56" bg="#F7FAFC"></Box>
+          <Box w="50%" h="56" bg="#F7FAFC"></Box>
+        </Flex>
+        <Box h="32" bg="#EDF2F7"></Box>
+      </Box>
+    </Flex>
   );
 };
 
